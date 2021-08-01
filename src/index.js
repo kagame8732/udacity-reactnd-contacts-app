bc27085d01db500f38859b72c90aa2ecd2108c58
kagame8732 <kagame8732@gmail.com>
2021-08-01 21:50:07 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class ContactList extends React.Component{
+  render(){
+    const person = [
+      {name:'Alex'},
+      {name:'Kagame'},
+      {name:'Ferg'}
+    ]
+    
+    const element = <ol>
+      {person.map((people) => (
+        <li key={people.name}>{people.name}</li>
+      ))
+      }
+    </ol>
+    
+    
+  }
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
-);
+)
+  
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
